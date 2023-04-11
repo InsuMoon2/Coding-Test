f74f28dfde0c9df06b896879a3e80deeffc39668
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-04-11 12:42:02 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) 
+{
+    string answer = "";
+    int index = 0;
+    
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ' ') 
+        {  
+            index = 0;
+            answer += ' ';
+        } 
+        
+        else 
+        {
+            if (index % 2 == 0) 
+            {  
+                answer += toupper(s[i]);
+            } 
+            
+            else
+            {  
+                answer += tolower(s[i]);
+            }
+            index++;
+        }
+    }
+    return answer;
+}
