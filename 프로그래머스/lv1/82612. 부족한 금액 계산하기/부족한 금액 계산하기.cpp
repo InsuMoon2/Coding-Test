030a207e1aa73b8e2e65c33ef497fb1eda8cc667
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-04-20 09:40:23 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+// 부족한 금액 계산하기
+
+long long solution(int price, int money, int count)
+{
+    long long answer = 0;
+
+    for (int i = 1; i <= count; i++)
+    {
+        answer += price * i;
+    }
+
+    if (answer < money)
+    {
+        answer = 0;
+    }
+
+    else
+    {
+        answer = answer - money;
+    }
+
+    return answer;
+}
