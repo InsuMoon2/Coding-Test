5f5485046f3512c5e8239605af06368566887dfd
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-04-20 11:11:22 +0900

    [level 2] Title: 숫자의 표현, Time: 0.04 ms, Memory: 3.92 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+// 숫자의 표현
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+    for (int i = 1; i <= n; i++) {
+        int sum = 0;
+        for (int j = i; j <= n; j++) {
+            sum += j;
+            if (sum == n) {
+                answer++;
+                break;
+            }
+            if (sum > n) {
+                break;
+            }
+        }
+    }
+    return answer;
+}
