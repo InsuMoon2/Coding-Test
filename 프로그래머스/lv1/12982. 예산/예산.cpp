8df32a4a62efee817130f5d563cd9382173e0b4b
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-04-23 17:01:31 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <cmath>
+
+// 예산
+
+using namespace std;
+
+int solution(vector<int> d, int budget)
+{
+    int answer = 0;
+    int sum = 0;
+
+    sort(d.begin(), d.end());
+
+    for (int i = 0; i < d.size(); i++)
+    {
+        sum += d[i];
+
+        if (sum > budget)
+        {
+            break;
+        }
+
+        answer++;
+    }
+
+    return answer;
+}
