2542b54ea1ed0531511fdc36263bf371134f9feb
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-04-26 15:31:51 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// 카카오 [1차] 비밀지도
+
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2)
+{
+    vector<string> answer;
+
+    for (int i = 0; i < n; i++)
+    {
+        string str = "";
+
+        arr1[i] = arr1[i] | arr2[i];
+
+        while (str.size() != n)
+        {
+            if (arr1[i] % 2 == 0)
+            {
+	            str.push_back(' ');
+            }
+
+            else
+            {
+	            str.push_back('#');
+            }
+
+            arr1[i] /= 2;
+        }
+
+        reverse(str.begin(), str.end());
+        answer.push_back(str);
+    }
+
+    return answer;
+}
+
+int main()
+{
+
+
+    return 0;
+}
