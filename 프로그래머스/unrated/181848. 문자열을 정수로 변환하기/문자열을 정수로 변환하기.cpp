9d0c23fade5bb8e70dc8a84c5e77c7f3bb54c422
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-04-28 12:18:43 +0900

    [unrated] Title: 문자열을 정수로 변환하기, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+// 문자열을 정수로 변환하기
+
+using namespace std;
+
+int solution(string n_str)
+{
+    int answer = 0;
+	answer = stoi(n_str);
+
+    return answer;
+}
