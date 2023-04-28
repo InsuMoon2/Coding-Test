a9fa65df264decb3427f24acb14b36313eca51c3
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-04-28 12:14:38 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// 소문자로 바꾸기
+
+using namespace std;
+
+string solution(string myString)
+{
+    string answer = "";
+
+    for (int i = 0; i < myString.size(); i++)
+    {
+        answer += toupper(myString[i]);
+    }
+
+    return answer;
+}
