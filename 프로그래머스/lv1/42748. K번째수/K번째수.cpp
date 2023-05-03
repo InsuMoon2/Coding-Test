9a26641d8cf282f254420c5ce18d1371ca9307f9
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-05-03 09:35:25 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// K번째 수
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands)
+{
+    vector<int> answer;
+
+    for (int i = 0; i < commands.size(); i++)
+    {
+        int start = commands[i][0] - 1;
+        int end = commands[i][1] - 1;
+        int k = commands[i][2] - 1;
+
+        vector<int> temp;
+
+	    for (int j = start; j <= end; j++)
+	    {
+            temp.push_back(array[j]);
+	    }
+
+        sort(temp.begin(), temp.end());
+
+        answer.push_back(temp[k]);
+    }
+
+    return answer;
+}
