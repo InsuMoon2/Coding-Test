55c34cc63f993fe8c27ba2e8e7932c67c0ae93ed
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-05-06 22:53:14 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -9,9 +9,11 @@ using namespace std;
 string solution(string myString)
 {
     string answer = "";
-    transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
 
-    answer = myString;
+    for (int i = 0; i < myString.size(); i++)
+    {
+        answer += tolower(myString[i]);
+    }
 
     return answer;
 }
