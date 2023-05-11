121aca47196776f19721d841fa2f8b138d6a181f
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-05-11 13:03:17 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+// N과 M, 백트래킹
+
+using namespace std;
+
+int N, M;
+int arr[9];
+bool visited[9];
+
+void DFS(int count)
+{
+	if (count == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << ' ';
+		}
+		cout << "\n";
+
+		
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (!visited[i])
+		{
+			visited[i] = true;
+			arr[count] = i;
+			DFS(count + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N >> M;
+
+    DFS(0);
+
+    return 0;
+}
+
