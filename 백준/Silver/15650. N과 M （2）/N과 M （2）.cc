a863e17efb790789c076bd235dd537da15b3b120
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-05-15 09:51:20 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+// N과 M (2)
+
+using namespace std;
+
+int N, M;
+int arr[9];
+bool visitied[9];
+
+void DFS(int num, int count)
+{
+	if (count == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = num; i <= N; i++)
+	{
+		if (!visitied[i])
+		{
+			visitied[i] = true;
+			arr[count] = i;
+			DFS(i, count + 1);
+			visitied[i] = false;
+		}
+	}
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N >> M;
+
+    DFS(1, 0);
+
+    return 0;
+}
+
