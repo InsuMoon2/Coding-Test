593838710e5867e0695c29f0443256fed070c79d
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-05-24 12:20:22 +0900

    [unrated] Title: 부분 문자열, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+// 부분 문자열
+
+using namespace std;
+
+int solution(string str1, string str2)
+{
+	int answer = 0;
+
+	if (str2.find(str1) == string::npos)
+	{
+		answer = 0;
+	}
+
+	else
+	{
+		answer = 1;
+	}
+
+	return answer;
+}
