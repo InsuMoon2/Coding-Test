a038fd0ef67cf5d8b46d81474499530860aed808
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-05-29 17:45:02 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    string S;
+    int R;
+
+    cin >> T;
+
+    while (T--)
+    {
+        cin >> R >> S;
+
+        for (int i = 0; i < S.size(); i++)
+        {
+            for (int j = 0; j < R; j++)
+            {
+                cout << S[i];
+            }
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
