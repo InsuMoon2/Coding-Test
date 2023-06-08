78a9a138332a81d9412f0affb16fd06b30080edb
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-06-08 09:51:14 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,52 +1,66 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cmath>
 #include <algorithm>
+#include <cmath>
+#include <map>
 #include <set>
 
 using namespace std;
 
+int arr[100'001] = {};
+
+int binary_search(int arr[], int target, int start, int end)
+{
+	while (start <= end)
+	{
+		int mid = (start + end) / 2;
+
+		if (arr[mid] == target)
+		{
+			return 1;
+		}
+
+		else if (arr[mid] > target)
+		{
+			end = mid - 1;
+		}
+
+		else if (arr[mid] < target)
+		{
+			start = mid + 1;
+		}
+	}
+
+	return 0; // 찾지 못했을 때
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(NULL);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    int N, M;
-    cin >> N;
+	int N, M;
+	cin >> N;
 
-    set<int> set;
-    vector<int> vec;
-    int number;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
 
-    for (int i = 0; i < N; i++)
-    {
-        int num;
-        cin >> num;
+	sort(arr, arr + N);
 
-        set.insert(num);
-    }
+	cin >> M;
 
-    cin >> M;
+	for (int j = 0; j < M; j++)
+	{
+		int target;
+		cin >> target;
 
-    for (int i = 0; i < M; i++)
-    {
-        int num;
-        cin >> num;
+		cout << binary_search(arr, target, 0, N - 1) << "\n";
+	}
 
-        vec.push_back(num);
 
-        if (set.find(vec[i]) != set.end())
-        {
-            cout << 1 << '\n';
-        }
-
-        else
-        {
-            cout << 0 << '\n';
-        }
-    }
-
-    return 0;
+	return 0;
 }
