e0470d7300fd3c4c06ba9a59a8eeba27885252f3
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-06-13 10:59:42 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+// 최댓값 만들기 (2)
+
+using namespace std;
+
+int solution(vector<int> numbers)
+{
+    int answer = 0;
+
+	sort(numbers.begin(), numbers.end());
+
+    int a = numbers[0] * numbers[1];
+    int b = numbers[numbers.size() - 1] * numbers[numbers.size() - 2];
+
+    answer = max(a, b);
+
+    return answer;
+}
