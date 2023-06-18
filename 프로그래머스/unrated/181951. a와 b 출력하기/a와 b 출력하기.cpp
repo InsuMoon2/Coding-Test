7528a4450a7f52817ca41b46dfb4fe6fe5eddfa4
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-06-19 00:54:26 +0900

    [unrated] Title: a와 b 출력하기, Time: 2.13 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// a와 b출력하기
+
+using namespace std;
+
+int main(void)
+{
+    int a;
+    int b;
+
+    cin >> a >> b;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+
+    return 0;
+}
