42f9286fa5388b14a21998107dc5e403df99326c
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-06-19 01:04:49 +0900

    [unrated] Title: 문자열 돌리기, Time: 1.54 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+// 문자열 돌리기
+
+using namespace std;
+
+int main(void)
+{
+    string str;
+    cin >> str;
+
+    for (int i = 0;  i < str.size(); i++)
+    {
+        cout << str[i] << endl;
+    }
+
+    return 0;
+}
