8d674f922a42ca9007413543a9c95f4c9a87d911
INSU MOON <75931369+InsuMoon2@users.noreply.github.com>
2023-06-29 12:55:50 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 0.18 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+// 수열과 구간 쿼리 2
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries)
+{
+    vector<int> answer;
+
+    for (auto vec : queries)
+    {
+        int val = 1000001;
+        for (int i = vec[0]; i <= vec[1]; i++)
+        {
+            if (arr[i] <= vec[2])
+                continue;
+
+            val = min(val, arr[i]);
+        }
+
+        if (val == 1000001)
+            val = -1;
+
+        answer.push_back(val);
+    }
+
+    return answer;
+}
